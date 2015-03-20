726ab37a3d870b90957044215a0ef7b575626adf
m28serj <serj.m28@gmail.com>
2015-03-20 13:21:12 +0200

    CodeStyle changes

--- app/Date.php
+++ app/Date.php
@@ -1,15 +1,17 @@
 <?php namespace App;
 
-class Date extends \M28Serj\Date\Date {
-		public function checkDate($offsetNextDay, $holidays){
-				if($this->isWeekend() || $holidays->has($this->day.'.'.$this->month)){
-						if($offsetNextDay){
-								return $this->addDay()->checkDate($offsetNextDay, $holidays);
-						} else {
-								return $this->subDay()->checkDate($offsetNextDay, $holidays);
-						}
-				} else {
-						return $this;
-				}
-		}
+class Date extends \M28Serj\Date\Date
+{
+    public function checkDate($offsetNextDay, $holidays)
+    {
+        if ($this->isWeekend() || $holidays->has($this->day . '.' . $this->month)) {
+            if ($offsetNextDay) {
+                return $this->addDay()->checkDate($offsetNextDay, $holidays);
+            } else {
+                return $this->subDay()->checkDate($offsetNextDay, $holidays);
+            }
+        } else {
+            return $this;
+        }
+    }
 }
